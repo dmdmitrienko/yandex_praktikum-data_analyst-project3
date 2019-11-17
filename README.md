d16be9f9ab4211440462426c11184633849b5f4c
mohovkm <mohovkm@ya.ru>
2019-11-17 12:50:05 +0500

    Third project for Yandex

---
+++ README.md
@@ -0,0 +1,1 @@
+# yandex_praktikum-data_analyst-project3
---
+++ install_requirements.sh
@@ -0,0 +1,11 @@
+#activate virtual environment
+python3 -m venv env
+source env/bin/activate
+
+#install packages
+pip3 install pandas
+pip3 install matplotlib
+pip3 install jupyterlab
+
+#run notebook
+jupyter notebook;
